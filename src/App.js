58aeda45730bd033fe4037f65d3d6f8697960f28
Hari <harikrishkk@gmail.com>
2023-01-26 14:21:13 -0500

    commit 3: base layout scaffold in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { Box } from '@mui/system';
+import React from 'react';
 import './App.css';
+import {
+  appWrapper,
+  contentWrapper,
+  sideBarWrapper,
+  contentAreaWrapper,
+  scrollListWrapper,
+  createAccountWrapper,
+  joinRedditWrapper,
+} from '@styles/styles';
 
 function App() {
-  return <div className="App">Hello react</div>;
+  return (
+    <React.Fragment>
+      <Box sx={appWrapper}>
+        {/* nav bar */}
+        <Box sx={contentWrapper}>
+          <Box sx={sideBarWrapper}>
+            <Box sx={scrollListWrapper}>{/* side bar list */}</Box>
+            <Box sx={createAccountWrapper}>
+              <Box sx={joinRedditWrapper}>{/* join reddit section */}</Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
 }
 
 export default App;
