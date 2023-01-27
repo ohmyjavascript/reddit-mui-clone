74efc9f7e1b6c8f481acd68860b9cf5ddaae982a
Hari <harikrishkk@gmail.com>
2023-01-27 11:06:45 -0500

    commit 12: join reddit bottom section

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import {
 } from '@styles/styles';
 import Navbar from '@navigation/Navbar';
 import SideList from '@components/SideList';
+import { blue, grey } from '@mui/material/colors';
+import { Typography } from '@mui/material';
+import CustomButton from '@shared/CustomButton';
 
 function App() {
   return (
@@ -24,7 +27,18 @@ function App() {
               <SideList />
             </Box>
             <Box sx={createAccountWrapper}>
-              <Box sx={joinRedditWrapper}>{/* join reddit section */}</Box>
+              <Box sx={joinRedditWrapper}>
+                <Typography sx={{ mb: 2 }} variant="body2" gutterBottom>
+                  Create an account to follow your favorite communities and
+                  start taking part in conversations.
+                </Typography>
+                <CustomButton
+                  bgColor={blue[300]}
+                  color={grey[900]}
+                  text="Join Reddit"
+                  fullWidth
+                />
+              </Box>
             </Box>
           </Box>
         </Box>
